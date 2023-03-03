a4199a0ea516534a6d51a80c3b77b1fdd9dc5b0f
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-03 11:15:09 +0200

    import-Notiflix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
